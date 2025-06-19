639dc10c0ccff700f29cb89610336770e80897da
Edmund Teo Chang Hao <ainurlindale@gmail.com>
2025-06-19 21:03:46 +0800

    setup prisma ORM and mySQL db

--- src/service/teacherService.ts
+++ src/service/teacherService.ts
@@ -1,5 +1,9 @@
-export const tempService = () => {
-  // do db call here
+import prisma from "../prisma/client";
 
+export const tempService = async () => {
+  // do db call here
+  const users = await prisma.user.findMany();
+
+  console.log("USERS", users);
   return null;
 };
